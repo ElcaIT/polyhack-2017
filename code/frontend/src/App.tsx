81697b57563f1c1478f4d760c4aa87e0480c623e
Martin Kempf <martin.kempf@elca.ch>
2017-09-26 15:21:49 +0200

    Add backend calls to get and create a person

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 import './App.css';
 import {PersonList} from './app/components/person/PersonList';
 import {AddPerson} from './app/components/person/AddPerson';
@@ -9,17 +10,28 @@ class App extends React.Component<any, any> {
         super(props, context);
         this.state = {
             persons: [
-                {name: 'Alex'},
-                {name: 'Martin'},
-                {name: 'Marco'}
+                // {name: 'Alex'},
+                // {name: 'Martin'},
+                // {name: 'Marco'}
             ]
         };
     }
 
+    componentDidMount() {
+        axios.get('./api/persons')
+            .then(response => {
+                this.setState({persons: response.data});
+            });
+    }
+
     addPerson(person) {
-        this.setState(prevState => ({
-            persons: [...prevState.persons, person]
-        }));
+        axios.post('./api/persons', person).then(response => {
+            this.setState(prevState => ({
+                persons: [...prevState.persons, response.data]
+            }));
+        }).catch(function (error: any) {
+            console.log(error);
+        });
     }
 
     render() {
