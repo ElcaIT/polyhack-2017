73ede5ffc292e9c7dd97e5c609f9cce4571940ac
Robert Weiser <robert.weiser@elca.ch>
2017-10-20 14:14:35 +0100

    Switching to more appropriate sample data

--- code/backend/src/main/resources/data.sql
+++ code/backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO person (id, version, name) VALUES (1, 1, 'H0mer Simps0n');
-INSERT INTO person (id, version, name) VALUES (2, 1, 'M@rge Simps0n');
+INSERT INTO person (id, version, name) VALUES (1, 1, 'Martin');
+INSERT INTO person (id, version, name) VALUES (2, 1, 'Robert');
